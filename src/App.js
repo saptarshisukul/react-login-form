b316682434188eb219c6cc16d4a478bc5c5b3286
saptarshisukul <saptarshi.sukul@gmail.com>
2023-02-22 17:08:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [track, setTrack] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const vaildateEmail = () => {
+    if (email.trim()) setTrack(1);
+    else alert("email required");
+  };
+
+  const vaildatePassword = () => {
+    if (password.trim()) alert("your credentials are submitted");
+    else alert("password required");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {track === 0 ? (
+        <section className="email-section">
+          <div className="email-div">
+            <label className="email-label" htmlFor="email">
+              Enter Your Email :
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(email) => {
+                setEmail(email.target.value);
+              }}
+            ></input>
+          </div>
+
+          <button onClick={vaildateEmail}>Validate Email</button>
+        </section>
+      ) : (
+        <section className="password-section">
+          <div>
+            <label className="password-label" htmlFor="password">
+              Enter Your Password :
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(p) => {
+                setPassword(p.target.value);
+              }}
+            ></input>
+          </div>
+          <button
+            onClick={() => {
+              setTrack(0);
+            }}
+          >
+            back
+          </button>
+          <button onClick={vaildatePassword}>Submit</button>
+        </section>
+      )}
     </div>
   );
 }
